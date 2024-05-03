2be04218a75236eb7980f39f299674ff1421d603
Nikhil Ranjan <nik.ranjancool@gmail.com>
2024-05-03 21:41:14 +0530

    Removed useState(), learnt and implemented useRef(), useState() causes many unnecessary paint cycles

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,41 +1,30 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 function AddTodo({ addTask }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
+  const nameInput = useRef();
+  const dateInput = useRef();
+
   const handleAddButtonClick = (event) => {
     event.preventDefault();
+    const todoName = nameInput.current.value;
+    const dueDate = dateInput.current.value;
+    nameInput.current.value = "";
+    dateInput.current.value = "";
     addTask(todoName, dueDate);
-    setDueDate("");
-    setTodoName("");
   };
   return (
     <div className="container text-center">
       <form className="row todo-row" onSubmit={handleAddButtonClick}>
         <div className="col-6">
           <input
+            name="todoName"
             type="text"
             placeholder="Enter todo here.."
-            id="task"
-            name="todo"
-            value={todoName}
-            onChange={handleNameChange}
+            ref={nameInput}
           />
         </div>
         <div className="col-4">
-          <input
-            type="date"
-            name="date"
-            id="date"
-            value={dueDate}
-            onChange={handleDateChange}
-          />
+          <input type="date" name="dueDate" ref={dateInput} />
         </div>
         <div className="col-2">
           <button type="submit" className="btn btn-primary todo-button add-btn">
