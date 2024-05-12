9bf6b59cc195be96ecaded99567b226629d18cb0
Nikhil Ranjan <nik.ranjancool@gmail.com>
2024-05-12 18:37:35 +0530

    Implemented useReducer and removed useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,45 @@ import AddTodo from "./components/AddTodo.jsx";
 
 import TodoItems from "./components/TodoItems.jsx";
 import "./App.css";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage.jsx";
 import { TodoItemsContext } from "./store/todo-items-store.jsx";
 
-function App() {
-  // const todoItems = [
-  //   {
-  //     name: "Go to work",
-  //     dueDate: "12/12/24",
-  //   },
-  //   {
-  //     name: "Buy groceries",
-  //     dueDate: "13/12/24",
-  //   },
-  //   {
-  //     name: "Make Youtube videos",
-  //     dueDate: "14/12/24",
-  //   },
-  // ];
-  const [todoItems, setTodoItems] = useState([]);
-  const addNewItem = (task, date) => {
-    // newtodo = {
-    //   task:task,
-    //   date:date
-    // }
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = currTodoItems;
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      { name: action.payload.task, date: action.payload.date },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter(
+      (item) => item.name !== action.payload.task
+    );
+  }
+  return newTodoItems;
+};
 
-    setTodoItems((currValue) => {
-      let newtodoItems = [...currValue, { name: task, dueDate: date }];
-      return newtodoItems;
-    });
+function App() {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+  const addNewItem = (task, date) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        task: task,
+        date: date,
+      },
+    };
+    dispatchTodoItems(newItemAction);
   };
-  const deleteItem = (todoItemName) => {
-    const newtodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newtodoItems);
+  const deleteItem = (task) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        task: task,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
   };
 
   return (
